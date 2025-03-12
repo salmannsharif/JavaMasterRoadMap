e71232a7405137bf2eb8a4ac6a4a63cdd65954e8
Salman Sharif <68875419+salmannsharif@users.noreply.github.com>
2025-03-12 14:24:18 +0530

    Update forClass.java

--- src/com/training/forClass.java
+++ src/com/training/forClass.java
@@ -2,10 +2,23 @@ package com.training;
 
 public class forClass {
     public static void main(String[] args) {
+        
         String[] data = "This is sample data".split(" ");
         for(int i = data.length - 1; i >= 0; i--) {
             System.out.print(data[i] + " ");
         }
+
+
+        // Reverese the string at the same place
+        String[] data = "This is sample data".split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : data) {
+            result.append(new StringBuilder(word).reverse()).append(" ");
+        }
+
+        // Printing final result
+        System.out.println(result.toString().trim());
 
     }
 }
