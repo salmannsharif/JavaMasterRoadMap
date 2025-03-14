204302c142bdcd90f74f36ad7a8ae1afbe7f3464
Salman Sharif <salmansheriff706@gmail.com>
2025-03-14 13:37:58 +0530

    Added InterviewPreparationQuestions class

---
+++ src/com/training/interview_preparation/InterviewPreparationQuestions.java
@@ -0,0 +1,24 @@
+package com.training.interview_preparation;
+
+public class InterviewPreparationQuestions {
+
+
+    void AboutPayFiniaProject(){
+        /**
+         * "In my previous role, I contributed to the development of Payfinia’s Instant Payment Xchange (IPX),
+         * a robust payment solution that enables banks and fintech companies to process instant transactions
+         * using the Federal Reserve’s FedNow® service. I worked on building and integrating APIs using Spring Boot,
+         * ensuring seamless connectivity between banking systems, the FedNow® network, and end-user applications.
+         * My responsibilities included implementing secure transaction workflows,
+         * fraud prevention mechanisms like OFAC(Office Foreign Asset Control)/AML(Anti Money Laundering)
+         * screening and transaction limits,
+         * and optimizing the system for real-time performance. This project gave me hands-on experience with ISO 20022 standards,
+         * open API development, and collaboration with core banking systems like XP2 and Fiserv DNA,
+         * all while delivering a scalable and secure solution for instant payments."
+         *
+         */
+    }
+
+
+
+}
