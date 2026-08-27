b7b9c8aa13ca2d6b3023fbf38b51ee7bc62e4dfa
salmannsharif <salmansheriff706@gmail.com>
2026-08-27 10:47:52 +0530

    Added removeEmptyStrings getHighestSalary methods

--- src/com/training/twoyearsexperiencedpreparation/Main.java
+++ src/com/training/twoyearsexperiencedpreparation/Main.java
@@ -4,6 +4,64 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+class Employee{
+    public int id;
+    public Long salary;
+    public String department;
+    public String address;
+
+    Employee(int id, Long salary, String department, String address){
+        this.id = id;
+        this.salary = salary;
+        this.department = department;
+        this.address = address;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
+
+
 public class Main {
 
 
@@ -84,6 +142,40 @@ public class Main {
         System.out.println(collect);
     }
 
+    static void removeEmptyStrings(){
+        String []  strArr = {"Java", "Python", "", "Dotnet", "NodeJS",};
+        String []  strArr1 = {"Java", "Python", " ", "", "Dotnet", "NodeJS"};
+        String []  strArr2 = {"Java", "Python", "Dotnet", "NodeJS", null};
+        Arrays.stream(strArr).filter(i-> !i.isEmpty()).forEach(System.out::println); // -> When array value has emptyString use this.
+        Arrays.stream(strArr1).filter(i-> !i.isBlank()).forEach(System.out::println); // -> When array value has empty space as value then use this.
+        Arrays.stream(strArr2).filter(Objects::nonNull).forEach(System.out::println); // -> When array value has null value then use this
+
+
+    }
+
+    static void getHighestSalary(){
+
+        Employee emp1 = new Employee(1, 10000L, "java", "us");
+        Employee emp2 = new Employee(2, 20000L, "python", "cn");
+        Employee emp3 = new Employee(3, 30000L, "ml", "uk");
+        Employee emp4 = new Employee(4, 50000L, "ai", "ru");
+        Employee emp5 = new Employee(5, 60000L, "llm", "in");
+        Employee emp6 = new Employee(6, 70000L, "da", "ir");
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(emp1);
+        employeeList.add(emp2);
+        employeeList.add(emp3);
+        employeeList.add(emp4);
+        employeeList.add(emp5);
+        employeeList.add(emp6);
+
+//        employeeList.stream().filter(data -> data.getSalary() < 30000).forEach(System.out::println); // Used to print employee object who get more than 30000.
+//        System.out.println(employeeList.stream().map(Employee::getSalary).sorted((data1, data2) -> (int) ((data2 - data1))).skip(2).findFirst().get());
+    }
+
+
+
     public static void main(String[] args) {
 //        moveZeroElementToRightInArray();
 //        moveZeroElementToRightInList();
@@ -93,5 +185,8 @@ public class Main {
 //        findingDuplicates();
 //        findingDuplicatesUsingStream();
 
+//        removeEmptyStrings();
+
+//        getHighestSalary();
     }
 }
